1030d6a30cfb9ed93824eef2210e0f324c417cab
dossa1020 <dossa0328@korea.ac.kr>
2018-10-01 11:48:32 +0900

    1. 처음 입력할때 띄어쓰기는 해결함 php의 get 방식의 request에서는 띄어쓰기를 넣고싶으면 '+' 가 필요했었음. 그런데..다른 문제가 생김.

--- message.php
+++ message.php
@@ -10,26 +10,22 @@ $info_array = array("의정보","의 유해성과 위험성","의응급조치요
 //$type = $data['type'];
 // $user_key = $data[
 // $content = '대화시작';
-$chemlist_chemid = Array();
 
+$chemlist_chemid = Array();
 $len_info_array = count($info_array);
+
 
 if(strpos($content,'#')!== false){
 	functions_afteraction($content);
 }
 
 if(strpos($content,'*') !== false){
-	//$content2 = (string)strstr($content,'*');
-	//$content2_divide = explode('*', $content2);
-	$content2 = str_replace("*","",$content);
+	$content2 = str_replace("*",'',$content);
 	
 	
 	for($len_i=0; $len_i<$len_info_array; $len_i++)
 	{
 		$info_array_output = $info_array_output.'#'.$content2.$info_array[$len_i].'&';
-		
-		//$chemlist = $chemlist.'*'.$x->chemNameKor.'&';
-		//$searchChemlist = explode('&', $chemlist);
 	}
 	$info_array_output = $info_array_output.'처음으로';
 	
@@ -40,10 +36,7 @@ if(strpos($content,'*') !== false){
 		array(
 			'message' => array(
 				'text' => "조회한 화학물질명은 [".$content2."] 입니다."
-//				'text' => $info_array_output
 			),
-			//'buttons' => array(
-			//'화학제품에 대한 정보','유해성, 위험성','응급조치 요령','폭발, 화재시 대처방법','누출사고시 대처방법','처음으로'
 			'keyboard' => array(
 				'type' => 'buttons',
 				'buttons' => $info_array_output_fianl
@@ -81,27 +74,14 @@ else if(strcmp($content,'화학 물질명 검색') == false){
 		)
 	);
 }
-
 else{
-//	$data = json_decode(file_get_contents('php://input'), true);
-	// 요청에서 content 항목 결정
-//	$content = $data["content"];
-	
-//	$content = "염산";
-
-	//대응 쿼리
-	//이 친구 순서 바꿔야함 
-//	$query_action = "http://msds.kosha.or.kr/openapi/service/msdschem/chemdetail04?chemId=001097&ServiceKey=QgcZ7AnmeeqX394vEJHPd7sO%2BdK6XCTAWBgvaoI7RLQXODtOpMYjr7lrDYgfRt863BqDPPpfQ4rL2C%2BROWMsUA%3D%3D";
-	//$query = "http://msds.kosha.or.kr/openapi/service/msdschem/chemlist?searchCnd=0&searchWrd=$content&ServiceKey=QgcZ7AnmeeqX394vEJHPd7sO%2BdK6XCTAWBgvaoI7RLQXODtOpMYjr7lrDYgfRt863BqDPPpfQ4rL2C%2BROWMsUA%3D%3D";
-	//sgxoTq3GeTy8Q9PzzEn9zG%2FnNmENGob0tYPeVglRFTmHRKYao7vVBdyt%2Bvml1ZYOS5YQcFpXdFu2Hr0jXdhsHg%3D%3D
-
 	//기본 정보 쿼리
-	$query_info = "http://msds.kosha.or.kr/openapi/service/msdschem/chemlist?searchCnd=0&searchWrd=$content&ServiceKey=QgcZ7AnmeeqX394vEJHPd7sO%2BdK6XCTAWBgvaoI7RLQXODtOpMYjr7lrDYgfRt863BqDPPpfQ4rL2C%2BROWMsUA%3D%3D";
+	$content_delplus = str_replace(" ",'+',$content);
+	$query_info = "http://msds.kosha.or.kr/openapi/service/msdschem/chemlist?searchCnd=0&searchWrd=$content_delplus&ServiceKey=QgcZ7AnmeeqX394vEJHPd7sO%2BdK6XCTAWBgvaoI7RLQXODtOpMYjr7lrDYgfRt863BqDPPpfQ4rL2C%2BROWMsUA%3D%3D";
+	
 	$myXMLData_info = file_get_contents($query_info);
 	$xml_info = simplexml_load_string($myXMLData_info) or die("Error: Cannot create object");
-
-//	$myXMLData_action = file_get_contents($query_action);
-//	$xml_action = simplexml_load_string($myXMLData_action) or die("Error : cannot create object");
+	
 	
 	$num = count($xml_info->body->items->item);
 	for ($i = 0; $i<$num ; $i++)
@@ -110,109 +90,26 @@ else{
 		$x = $xml_info->body->items->item[$i];
 		
 		$chemlist = $chemlist.'*'.$x->chemNameKor.'&';
-		//print_r($x);
 		$chemName_input = (string)($x->chemNameKor);
 		$chemId_input = (string)($x->chemId);
 		$chemlist_chemid[$chemName_input] = $chemId_input;
 		
-		//print_r($chemlist_chemid);
-		
-		/*
-		$x = $xml_info->body->items->item[$i];
-		$x_n = $xml_info->body->items->item->chemId;
-	
-		$query_action = "http://msds.kosha.or.kr/openapi/service/msdschem/chemdetail04?chemId=$x_n&ServiceKey=QgcZ7AnmeeqX394vEJHPd7sO%2BdK6XCTAWBgvaoI7RLQXODtOpMYjr7lrDYgfRt863BqDPPpfQ4rL2C%2BROWMsUA%3D%3D";
-		$myXMLData_action = file_get_contents($query_action);
-		$xml_action = simplexml_load_string($myXMLData_action) or die("Error : cannot create object");
-
-		$k = $xml_action->body->items->item[$i];
-
-		//$sum_info = $sum_info.(string)"casNo -> ".$x->casNo."chemId -> ".$x->chemId."---------".$k->itemDetail.'\n';
-		$sum_info = $sum_info."casNo -> ".$x->casNo."chemId -> ".$x->chemId."---------".$k->itemDetail.'\n';
-		$sum_act = (string)"how to action after accident -> ".$x->itemDetail.'<br>';
-		*/
 	}
-	//echo $chemName_input. '%%%%%' . $chemId_input;
-	//echo "dlrjsadjfaksdf -> ".$chemlist_chemid['벤젠'];
-	
 	$chemlist = $chemlist.'처음으로';
 	$searchChemlist = explode('&', $chemlist);
-
-
-	//$bu = array("1","2","3");
+	
 	echo json_encode(
 		array(
 			'message' => array(
 				'text' => "검색한 화학물질은 ".$content."입니다."."\n"."검색된 화학물질명이 포함된 물질은 총 ".$num."개 입니다. 조회를 원하는 물질명을 선택해주세요."
-				//'text' => $chemlist_chemid
 			),
 			'keyboard' => array(
 				'type' => 'buttons',
-//				'buttons' => array(
-//				'염산 오라민','염산 구아니딘','염산 디에틸아민','염산 에탄올아민','염산 L-시스틴','염산 프로티오카브','데메클로사이클린 염산염','L-글루타민 산','아미노비페닐 염산염','나트륨 하이포아염산염 오수화물',$tt,$ttt,'처음으로'
 				'buttons' => $searchChemlist
-				//'buttons' => $chemlist_chemid
 				
 			)
 		)
 	);
-	/*if(strpos($searchChemlist,$content)){
-		echo json_encode(
-			array(
-				'message' => array(
-					'text' => "조회할 물질 : ".$content
-				)
-			)
-		);
-	}*/
-	/*
-	
-	//'화학제품에 대한 정보','유해성, 위험성','응급조치 요령','폭발, 화재시 대처방법','누출사고시 대처방법','처음으로'
-	if(strcmp($content,'화학제품에 대한 정보')==false){
-		functions_afteraction();
-	}
-	else if(strcmp($content,'유해성, 위험성')==false){
-		functions_afteraction();
-	}
-	else if(strcmp($content,'응급조치 요령')==false){
-		functions_afteraction();
-	}
-	else if(strcmp($content,'폭발, 화재시 대처방법')==false){
-		functions_afteraction();
-	}
-	else if(strcmp($content,'누출사고시 대처방법')==false){
-		functions_afteraction();
-	}
-	else if (strcmp($content, '적절한(부적절한)소화제') == false){
-		echo json_encode(
-					array(
-							'message' => array(
-									'text' => '적절한 소화제는 까스활명수'
-							),
-							'keyboard' => array(
-									'type' => 'buttons',
-									'buttons' => array(
-									'소화제는 없습니다','처음으로','소화제 있음'
-									)
-							)
-					)
-			);
-	}
-	else{
-		echo json_encode(
-					array(
-							'message' => array(
-									'text' => '오류 발생임.'
-							),
-							'keyboard' => array(
-									'type' => 'buttons',
-									'buttons' => array(
-											'처음으로'
-									)
-							)
-					)
-			);
-	}
-	*/
+
 }
 ?>
